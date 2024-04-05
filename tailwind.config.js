00f5488c27e791dd3e97eb92b1ae1be8d52f00e9
tanishq <tanishqvideo@gmail.com>
2024-04-05 12:39:19 +0530

    :home page completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'C6DCE5': '#C6DCE5',
+        'C6DCE5': '#C6DCE5', '3D3D4E': '#3D3D4E','282E32': '#282E32' , '2A2A32': '#2A2A32'
       },
     },
   },
