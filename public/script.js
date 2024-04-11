a6e3a49c9cf8984456a0cb2aafb116a48d8a924e
tanishq <tanishqvideo@gmail.com>
2024-04-11 10:08:53 +0530

    navbar almost done

--- public/script.js
+++ public/script.js
@@ -1,16 +1,11 @@
-const homebutton=document.getElementById('homebutton')
-// const tempimg = document.getElementById('tempimg')
-
-
-
-// const title1st= document.getElementById('title1st')
-// const columns=document.getElementById('columns')
-// const projectstitle=document.querySelector('projectstitle')
-// const line = document.getElementById('line')
-// const edits= document.getElementById('edits')
-
-
+const homebutton=document.querySelector('.homebutton')
 const main=document.querySelectorAll('.main')
+const userselected= document.getElementById('userselected')
+const profilebutton= document.getElementById('user');
+const hb_ns=document.getElementById("homebutton_ns");
+const hb_span=document.getElementById('home_span');
+const pb_span=document.getElementById('profile_span');
+const indicator_id = document.getElementById('indicator_id');
 
 const hideElements = ()=>{
     main.forEach((Elements)=>Elements.classList.add("hidden"))
@@ -19,18 +14,40 @@ const showElements = ()=> {
     main.forEach((Elements)=>Elements.classList.remove('hidden'))
 }
 
-const userselected= document.getElementById('userselected')
 
-const profilebutton= document.querySelector('.profile')
+homebutton.addEventListener('click', (e) => {
+    showElements();
+});
+
 profilebutton.addEventListener('click',(e)=>{
     e.preventDefault();
-    profilebutton.classList.add('show');
+    document.getElementById('userselected').classList.remove('hidden')
     hideElements();
     userselected.classList.remove('hidden');
     profilebutton.classList.add('hidden');
+
+    document.getElementById("homebutton_ns").classList.remove('hidden')
+    homebutton.classList.add('hidden')
+
+    hb_ns.style.opacity = '0.75';
+    hb_span.style.opacity='0.75';
+    hb_span.style.fontWeight='300';
+    pb_span.style.opacity='1'
+    pb_span.style.fontWeight='500'
+
+    indicator_id.style.left= '2.4px'
+
 });
 
-
-homebutton.addEventListener('click', ()=>{
+hb_ns.addEventListener('click', ()=>{
     showElements();
-})
+    userselected.classList.add('hidden')
+    profilebutton.classList.remove('hidden')
+    document.getElementById("homebutton_ns").classList.add('hidden')
+    homebutton.classList.remove('hidden')
+    hb_span.style.opacity='1'
+    hb_span.style.fontWeight='500'
+    pb_span.style.opacity='0.75'
+    pb_span.style.fontWeight='300'
+    indicator_id.style.left= '0.5px'
+});
