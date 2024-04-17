307e4649308cac495bb32e373c85aec7410c94bd
tanishq <tanishqvideo@gmail.com>
2024-04-18 01:53:50 +0530

    dark mode

--- public/script.js
+++ public/script.js
@@ -1,4 +1,3 @@
-  
 const homebutton=document.querySelector('.homebutton')
 const main=document.querySelectorAll('.main')
 const userselected= document.getElementById('userselected')
@@ -10,6 +9,44 @@ const indicator_id = document.getElementById('indicator_id');
 const navbar = document.getElementById('navbar');
 const indicator_content= document.getElementById('indicator_content')
 const indicator_back = document.getElementById('indicator_back')
+const indicator_helper = document.getElementById('indicator_helper');
+
+
+
+// navbar icons
+const signalhigh_white = document.getElementById('signalhigh_white')
+const signalhigh_black = document.getElementById('signalhigh_black')
+
+const batterylow_white = document.getElementById('batterylow_white')
+const batterylow_black = document.getElementById('batterylow_black')
+
+const search_white = document.getElementById('search_white')
+const search_black = document.getElementById('search_black')
+
+const sliders_horizontal_black = document.getElementById('sliders_horizontal_black')
+const sliders_horizontal_white = document.getElementById('sliders_horizontal_white')
+
+
+const ellipsis_white = document.getElementById('ellipsis_white')
+const ellipsis_black = document.getElementById('ellipsis_black')
+
+
+
+
+const image = document.createElement('img');
+
+
+image.src = 'assets/ellipsis1.svg'; 
+image.id = 'ellipsis_black';
+image.className = 'absolute left-[350px] cursor-pointer hidden';
+
+const container = document.getElementById('edit2');
+
+
+container.appendChild(image);
+
+
+
 
 const hideElements = ()=>{
     main.forEach((Elements)=>Elements.classList.add("hidden"))
@@ -129,3 +166,109 @@ indicator_back.addEventListener('click',()=>{
 
 // scrolling try
 
+const checkbox = document.getElementById('check');
+const mobiletabnav= document.getElementById('mobiletabnav')
+
+// const spans = ()=>{
+//     const spanss = document.getElementsByTagName('span')
+//     main.forEach((spanss)=spanss.style.color='black');
+//     console.log(spanss);
+// }
+
+
+
+// checkbox.addEventListener('click', ()=>{
+//     mobiletabnav.style.background='white'
+//     console.log(mobiletabnav);
+//     spans.style.color='black';
+//     console.log(spans)
+// })
+
+// const spans = Array.from(document.querySelectorAll('span'))
+const spans= document.querySelectorAll('span')
+const h1_tags = document.querySelectorAll('h1');
+
+const random_line = document.querySelector('#line')
+
+function changeContent(){
+    if(checkbox.checked){
+        mobiletabnav.style.backgroundColor='white';
+
+        spans.forEach(Element=>{
+            Element.style.color='black'
+        })
+
+        navbar.style.backgroundColor='#E7E7E7'
+        // navbar.style.backgroundColor='#F2F3F5'
+        h1_tags.forEach(Element=>{
+            Element.style.color='black'
+        })
+
+        random_line.style.backgroundColor='#E7E7E7'
+
+        indicator_helper.style.backgroundColor='white'
+
+        signalhigh_black.classList.remove('hidden')
+        signalhigh_white.classList.add('hidden')
+
+
+        batterylow_black.classList.remove('hidden')
+        batterylow_white.classList.add('hidden')
+
+
+        search_black.classList.remove('hidden')
+        search_white.classList.add('hidden')
+
+        sliders_horizontal_black.classList.remove('hidden')
+        sliders_horizontal_white.classList.add('hidden')
+
+
+        ellipsis_black.classList.remove('hidden')
+        ellipsis_white.classList.add('hidden')
+
+        
+        console.log(container)
+
+
+    }
+    else{
+        mobiletabnav.style.backgroundColor = '#202125';
+
+        spans.forEach(Element=>{
+            Element.style.color='white'
+        })
+
+        navbar.style.backgroundColor='#303135'
+
+
+        h1_tags.forEach(Element=>{
+            Element.style.color='white '
+        })
+
+        random_line.style.backgroundColor='#303135'
+
+        indicator_helper.style.backgroundColor='#202125'
+
+        signalhigh_black.classList.add('hidden')
+        signalhigh_white.classList.remove('hidden')
+
+
+        batterylow_black.classList.add('hidden')
+        batterylow_white.classList.remove('hidden')
+
+
+        search_black.classList.add('hidden')
+        search_white.classList.remove('hidden')
+
+        sliders_horizontal_black.classList.add('hidden')
+        sliders_horizontal_white.classList.remove('hidden')
+
+
+        ellipsis_black.classList.add('hidden')
+        ellipsis_white.classList.remove('hidden')
+
+
+    }
+}
+
+checkbox.addEventListener('change', changeContent)
